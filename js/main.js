d2bffe419f02019bf4ee306403224ce1407fd358
Monika <412.kri@gmail.com>
2023-06-05 21:39:30 +0300

    fetch

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@
 
 const ulDOM = document.querySelector('ul');
 
-const data = [];
+const data = fetch('https://m1r4cl38.github.io/fetch//data/users.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        for (const user of data) {
+            console.log(user);
+        }
+    });
 
-let HTML = '';
 
-for (const item of data) {
+//let HTML = '';
+
+//for (const item of data) {
     HTML += `<li>${item.id}) ${item.text}</li>`;
-}
+//}
 
-ulDOM.innerHTML = HTML;
+//ulDOM.innerHTML = HTML;
